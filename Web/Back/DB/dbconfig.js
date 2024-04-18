02306b8b0193738c3fed95df685dd13923560a07
JUNG <97083703+Jung2312@users.noreply.github.com>
2024-04-18 23:19:42 +0900

    Update dbconfig.js

--- Web/Back/DB/dbconfig.js
+++ Web/Back/DB/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: "c##aiproject",
-  password: "ai1234ai",
-  connectString: "122.38.11.25:1521/xe",
+  user: "user",
+  password: "pw",
+  connectString: "ip/xe",
   externalAuth: false,
 };
