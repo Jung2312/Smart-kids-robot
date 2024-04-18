ea9bd111857dc9edfca5852ea20e7857ab954bc8
JUNG <20212856@office.deu.ac.kr>
2024-04-18 23:16:38 +0900

    feat : add db_connect
    feat : add db_connect

---
+++ Web/Back/DB/dbconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  user: "c##aiproject",
+  password: "ai1234ai",
+  connectString: "122.38.11.25:1521/xe",
+  externalAuth: false,
+};
---
+++ Web/Back/db_connect.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const oracledb = require("oracledb");
+const dbConfig = require("./DB/dbconfig");
+
+const router = express.Router();
+async function selectDatabase(query) {
+  try {
+    console.log("db 연결");
+    let connection = await oracledb.getConnection(dbConfig);
+
+    let binds = {};
+    let options = {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,
+    };
+
+    let result = await connection.execute(query, binds, options);
+
+    console.log(result.rows);
+
+    await connection.close();
+
+    return result.rows;
+  } catch (error) {
+    console.error("오류 발생:", error);
+    throw error;
+  }
+}
+
+router.get("/db", async function (req, res) {
+  try {
+    const education = req.query.education; // 한글, 영어, 퀴즈
+    const category = req.query.category; // 자음, 모음, 쌍자음, 단어, 알파벳, 동물, 과일, 교통
+    let query = null;
+
+    if (education == "wordQuiz") {
+      query = `SELECT COUNT(*) FROM ${education} where category = ${category}`;
+    } else {
+      query = `SELECT COUNT(*) FROM ${education} where ${category} IS NOT NULL`;
+    }
+    const result = await selectDatabase(query);
+
+    const resultWithUrls = await processImages(result);
+
+    res.json({
+      word_img: resultWithUrls,
+    });
+  } catch (error) {
+    console.error("db error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
